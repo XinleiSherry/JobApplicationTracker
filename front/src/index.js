f2e856c5936a2debc31bcf7020e15c0f58262fec
Sherry <xinlei1207@gmail.com>
2022-11-20 01:18:49 -0800

    edit json

--- front/src/index.js
+++ front/src/index.js
@@ -5,6 +5,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import { Provider } from 'react-redux'
 import store  from './store';
+import "./components/Page/page.css"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
